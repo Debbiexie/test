e9dfc722df3534236aea6040395fa3107ce7f1a5
Debbiexie <debbie.dy.xie@gmail.com>
2022-02-13 01:44:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const http=require('http')
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain'})
 	res.end('debi first web mp2')
-}).listen(3000)
+}).listen(8089)
 
-console.log('server runing on 8081')
+console.log('server runing on 8089')
