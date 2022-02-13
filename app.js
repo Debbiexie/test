e77eb3b75376eee8df708d5f6e9803e9284804a8
Debbiexie <debbie.dy.xie@gmail.com>
2022-02-13 15:15:32 +0800

    Update app.js
    kvhk

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http=require('http')
 
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain'})
-	res.end('debi first web mp2')
+	res.end('debi first web mp2 ttt')
 }).listen(8089)
 
-console.log('push test github sh pull')
+console.log('push test + github sh pull')
