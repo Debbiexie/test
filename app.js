8ea6bc694f285f273a633ed99f004d44f70b2291
debbie xie <debbie.x@shopperplus.com>
2022-04-02 12:02:53 +0800

    add git source

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http=require('http')
 
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain'})
-	res.end('github push ,docker jenkin github test remote git source')
+	res.end('github push ,docker jenkin github test have to add git source')
 }).listen(8089)
 
 console.log('lightho ')
