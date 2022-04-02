1655893c4ce3ac6c7a46015f754e402e1efe21ea
debbie xie <debbie.x@shopperplus.com>
2022-04-02 11:08:44 +0800

    window push

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http=require('http')
 
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type':'text/plain'})
-	res.end('github push ,then via pm2 restart app test again')
+	res.end('github push ,docker jenkin github test')
 }).listen(8089)
 
 console.log('lightho ')
